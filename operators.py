813c5f7653b362229cdca23b5507131537307d3c
marcelabeckmann <marcela.bkmn@gmail.com>
2023-04-24 20:24:30 -0600

    ADD: OPERATORS

---
+++ operators.py
@@ -0,0 +1,21 @@
+addition = 10 + 5 #esto dara como resultado 15
+substraction = 10 - 5 #dara 5
+multiplicacion = 10 * 5 #dara 50
+
+#Comparision operators
+age = 10 == 5 # dara false
+age = 10 != 5 # dara true
+age = 10 > 5 # dara true
+
+#logical operators
+age_boy=10
+age_girl=14
+
+age_boy > 8 and age_girl > 8 #true
+
+#assignment opertors
+age = 10 
+
+age += 5 #dara 15
+age -= 5 #dara 5
+age *= 5 #dara 50

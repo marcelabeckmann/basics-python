4b944d4a328ab4aee92111c637d5e13663dddf74
marcelabeckmann <marcela.bkmn@gmail.com>
2023-04-28 16:34:56 -0600

    UPD: Convert integer to string

--- variables.py
+++ variables.py
@@ -1,7 +1,10 @@
 name = "Marcela"
+last_name="Beckmann"
 age = 23
 
 list_shopping = ["Milk", "Eggs", "Vegetables"]
 
-print(f"{name} {age}")
+print(f"{name} {last_name} {age}")
+
+print(name + " " + last_name + " " +str(age) )
 print(list_shopping)
